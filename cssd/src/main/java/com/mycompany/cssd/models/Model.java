65e7cc50cb5b7a16bebd62481ae385e2702379d7
Hamdan <21hamdanas@gmail.com>
2021-05-04 15:15:54 +0700

    Creating save, update, and destroy method. Not tested yet.

--- cssd/src/main/java/com/mycompany/cssd/models/Model.java
+++ cssd/src/main/java/com/mycompany/cssd/models/Model.java
@@ -10,37 +10,91 @@ import java.sql.SQLException;
  */
 public class Model {
     protected String table;
-    protected String[] fillable;
+    protected String[] fillables;
     
-    private static Database db;
+    private Database db;
     
-    protected Model(){
-        table = "";
-        fillable = new String[]{};
+    public int save(String[] data){
+        try {
+            String questionMark = "?";
+            db = new Database();
+
+            for (String fillable : fillables) {
+                questionMark += ",?";
+            }
+
+            String query = "insert into " + table + " values(" + questionMark + ")";
+            db.query(query);
+            PreparedStatement stmt = db.getStmt();
+
+            stmt.setInt(1, 0);
+            for (int i = 1; i <= fillables.length - 1; i++){
+                stmt.setString(i + 1, data[i - 1]);
+            }
+
+            stmt.execute();
+
+            db.getConn().close();
+
+            return 1;
+        
+        } catch (SQLException e){
+            return 0;
+        }
     }
     
-    public static void save() throws SQLException{
-        db = new Database();
-        String query = "insert into stokgudang values(?,?,?,?,?)";
-        db.query(query);
-        PreparedStatement stmt = db.getStmt();
-        
-        stmt.setInt(1, 0);
-        stmt.setString(2, "ADFda");
-        stmt.setString(3, "ASDFsa");
-        stmt.setString(4, "2020-1-1");
-        stmt.setString(5, "15");
-        
-        stmt.execute();
-        
-        db.getConn().close();
+    protected int update(String[] data, int id){
+        try{
+            String dataSection = "";
+            db = new Database();
+
+            for (String fillable : fillables) {
+                dataSection += ", " + fillable + "=?";
+            }
+
+            char[] dataSectionArray = dataSection.toCharArray();
+            dataSectionArray[0] = ' ';
+
+            String newDataSection = String.valueOf(dataSectionArray)
+                .replaceAll("\\s+", " ")
+                .trim();
+
+            String query = "update " + table + " set " + newDataSection + " where id=?";
+            db.query(query);
+            PreparedStatement stmt = db.getStmt();
+
+            for (int i = 1; i <= fillables.length - 1; i++){
+                stmt.setString(i, data[i - 1]);
+            }
+            stmt.setInt(fillables.length - 1, id);
+
+            stmt.execute();
+
+            db.getConn().close();
+            
+            return 1;
+        } catch(SQLException e) {
+            return 0;
+        }
+//
     }
     
-    protected static void update(){
-        
-    }
-    
-    protected static void destroy(){
-        
+    protected int destroy(int id){
+        try{
+            db = new Database();
+            
+            String query = "delete from " + table + "where id = ?";
+            db.query(query);
+            PreparedStatement stmt = db.getStmt();
+            
+            stmt.setInt(1, id);
+            
+            stmt.execute();
+            
+            db.getConn().close();
+            return 1;
+        } catch (SQLException e){
+            return 0;
+        }
     }
 }

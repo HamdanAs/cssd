1c1cac21e8df3cb55f159cb478a7c54aeff9e60f
hamdanas21 <21hamdanas@gmail.com>
2021-05-06 09:17:06 +0700

    create controller interface

---
+++ src/main/java/com/mycompany/cssd/controllers/Controller.java
@@ -0,0 +1,13 @@
+package com.mycompany.cssd.controllers;
+
+/**
+ *
+ * @author NESAS
+ */
+public interface Controller {
+    public void insert();
+    public void update();
+    public void delete();
+    public void showAll();
+    public void showById();
+}
--- src/main/java/com/mycompany/cssd/models/Model.java
+++ src/main/java/com/mycompany/cssd/models/Model.java
@@ -3,6 +3,8 @@ package com.mycompany.cssd.models;
 import com.mycompany.cssd.database.Database;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -10,32 +12,95 @@ import java.sql.SQLException;
  */
 public class Model {
     protected String table;
-    protected String[] fillable;
+    protected String[] fillables;
     
-    private static Database db;
+    private Database db;
     
-    public static void save() throws SQLException{
-        db = new Database();
-        String query = "insert into stokgudang values(?,?,?,?,?)";
-        db.query(query);
-        PreparedStatement stmt = db.getStmt();
-        
-        stmt.setInt(1, 0);
-        stmt.setString(2, "ADFda");
-        stmt.setString(3, "ASDFsa");
-        stmt.setString(4, "2020-1-1");
-        stmt.setString(5, "15");
-        
-        stmt.execute();
-        
-        db.getConn().close();
+    public int save(String[] data){
+        try {
+            String questionMark = "?";
+            db = new Database();
+
+            System.out.println(Arrays.toString(this.fillables));
+            
+            for (String fillable : this.fillables) {
+                questionMark += ",?";
+            }
+
+            String query = "insert into " + table + " values(" + questionMark + ")";
+            System.out.println(query);
+            db.query(query);
+            PreparedStatement stmt = db.getStmt();
+
+            stmt.setInt(1, 0);
+            for (int i = 1; i <= this.fillables.length; i++){
+                stmt.setString(i + 1, data[i - 1]);
+            }
+
+            stmt.execute();
+
+            db.getConn().close();
+
+            return 1;
+        } catch (SQLException e){
+            return 0;
+        }
     }
     
-    protected static void update(){
-        
+    public int update(String[] data, int id){
+        try{
+            String dataSection = "";
+            db = new Database();
+
+            for (String fillable : this.fillables) {
+                dataSection += ", " + fillable + "=?";
+            }
+
+            char[] dataSectionArray = dataSection.toCharArray();
+            dataSectionArray[0] = ' ';
+
+            String newDataSection = String.valueOf(dataSectionArray)
+                .replaceAll("\\s+", " ")
+                .trim();
+
+            String query = "update " + table + " set " + newDataSection + " where id=?";
+            db.query(query);
+            PreparedStatement stmt = db.getStmt();
+
+            for (int i = 1; i <= this.fillables.length; i++){
+                stmt.setString(i, data[i - 1]);
+            }
+            stmt.setInt(this.fillables.length + 1, id);
+
+            stmt.execute();
+
+            db.getConn().close();
+            
+            return 1;
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+            return 0;
+        }
+//
     }
     
-    protected static void destroy(){
-        
+    public int destroy(int id){
+        try{
+            db = new Database();
+            
+            String query = "delete from " + table + " where id = ?";
+            db.query(query);
+            PreparedStatement stmt = db.getStmt();
+            
+            stmt.setInt(1, id);
+            
+            stmt.execute();
+            
+            db.getConn().close();
+            return 1;
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return 0;
+        }
     }
 }

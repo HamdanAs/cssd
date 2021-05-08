2870f2c606a2c3608a9df36241a3a24b0880c652
hamdanas21 <21hamdanas@gmail.com>
2021-05-08 15:28:25 +0700

    create ruangan group views

--- src/main/java/com/mycompany/cssd/form/frmRuangan.java
+++ src/main/java/com/mycompany/cssd/form/frmRuangan.java
@@ -5,6 +5,9 @@
  */
 package com.mycompany.cssd.form;
 
+import com.mycompany.cssd.utility.Table;
+import javax.swing.JPanel;
+
 /**
  *
  * @author NESAS
@@ -14,8 +17,25 @@ public class frmRuangan extends javax.swing.JFrame {
     /**
      * Creates new form frmRuangan
      */
+    
+    private Table table;
+    
+    private JPanel[] panels;
     public frmRuangan() {
         initComponents();
+        
+        init();
+    }
+    
+    private void init(){
+        setLocationRelativeTo(null);
+        
+//        Variables
+        String[] column = {"ID", "Nama Ruangan", "Status"};        
+        
+        table = new Table(tblData);
+        table.setColumn(column);
+        table.setColumnWidth(742, 10, 70, 20);
     }
 
     /**
@@ -28,67 +48,108 @@ public class frmRuangan extends javax.swing.JFrame {
     private void initComponents() {
 
         jSplitPane1 = new javax.swing.JSplitPane();
-        jPanel1 = new javax.swing.JPanel();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
+        pnlNav = new javax.swing.JPanel();
+        btnList = new javax.swing.JButton();
+        pnlCards = new javax.swing.JPanel();
+        pnlList = new javax.swing.JPanel();
+        jButton4 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblData = new javax.swing.JTable();
+        jTextField1 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jSplitPane1.setBorder(null);
+        jSplitPane1.setDividerSize(1);
         jSplitPane1.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
 
-        jPanel1.setBackground(new java.awt.Color(225, 241, 221));
+        pnlNav.setBackground(new java.awt.Color(225, 241, 221));
 
-        jButton2.setBackground(new java.awt.Color(225, 241, 221));
-        jButton2.setFont(new java.awt.Font("Poppins", 1, 18)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(118, 97, 97));
-        jButton2.setText("TAMBAH RUANGAN");
-        jButton2.setBorder(null);
+        btnList.setBackground(new java.awt.Color(225, 241, 221));
+        btnList.setFont(new java.awt.Font("Poppins", 1, 18)); // NOI18N
+        btnList.setForeground(new java.awt.Color(118, 97, 97));
+        btnList.setText("LIST RUANGAN");
+        btnList.setBorder(null);
+        btnList.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnListMouseClicked(evt);
+            }
+        });
 
-        jButton3.setBackground(new java.awt.Color(225, 241, 221));
-        jButton3.setFont(new java.awt.Font("Poppins", 1, 18)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(118, 97, 97));
-        jButton3.setText("CARI RUANGAN");
-        jButton3.setBorder(null);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        javax.swing.GroupLayout pnlNavLayout = new javax.swing.GroupLayout(pnlNav);
+        pnlNav.setLayout(pnlNavLayout);
+        pnlNavLayout.setHorizontalGroup(
+            pnlNavLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlNavLayout.createSequentialGroup()
                 .addGap(37, 37, 37)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(293, Short.MAX_VALUE))
+                .addComponent(btnList, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(518, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        pnlNavLayout.setVerticalGroup(
+            pnlNavLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlNavLayout.createSequentialGroup()
                 .addGap(36, 36, 36)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
+                .addComponent(btnList)
                 .addContainerGap(35, Short.MAX_VALUE))
         );
 
-        jSplitPane1.setLeftComponent(jPanel1);
+        jSplitPane1.setLeftComponent(pnlNav);
 
-        jPanel2.setBackground(new java.awt.Color(135, 167, 179));
+        pnlCards.setLayout(new java.awt.CardLayout());
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 762, Short.MAX_VALUE)
+        pnlList.setBackground(new java.awt.Color(135, 167, 179));
+
+        jButton4.setBackground(new java.awt.Color(225, 241, 221));
+        jButton4.setFont(new java.awt.Font("Poppins", 1, 18)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(118, 97, 97));
+        jButton4.setText("TAMBAH RUANGAN");
+        jButton4.setBorder(null);
+
+        tblData.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tblData);
+
+        jTextField1.setFont(new java.awt.Font("Poppins", 1, 14)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(118, 97, 97));
+
+        javax.swing.GroupLayout pnlListLayout = new javax.swing.GroupLayout(pnlList);
+        pnlList.setLayout(pnlListLayout);
+        pnlListLayout.setHorizontalGroup(
+            pnlListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlListLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnlListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 742, Short.MAX_VALUE)
+                    .addGroup(pnlListLayout.createSequentialGroup()
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 560, Short.MAX_VALUE)
+        pnlListLayout.setVerticalGroup(
+            pnlListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlListLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnlListLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton4)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 502, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
-        jSplitPane1.setRightComponent(jPanel2);
+        pnlCards.add(pnlList, "card2");
+
+        jSplitPane1.setRightComponent(pnlCards);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -103,6 +164,10 @@ public class frmRuangan extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnListMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnListMouseClicked
+        
+    }//GEN-LAST:event_btnListMouseClicked
 
     /**
      * @param args the command line arguments
@@ -140,10 +205,14 @@ public class frmRuangan extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton btnList;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JPanel pnlCards;
+    private javax.swing.JPanel pnlList;
+    private javax.swing.JPanel pnlNav;
+    private javax.swing.JTable tblData;
     // End of variables declaration//GEN-END:variables
 }
